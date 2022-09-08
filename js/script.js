67e3943b809b4193001c659798b246a6e072fa91
Vegard M <94170927+CodeVegard@users.noreply.github.com>
2022-09-08 10:42:00 +0200

    JavaScript MA1
    Originally written directly in the ma-javascript-1-module1 repo. Cloned all files over here afterwards

---
+++ js/script.js
@@ -0,0 +1,73 @@
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+//Question 1:
+const cat = {
+    complain: function catComplain() {
+        console.log("Meow");
+    }
+};
+
+//Question 2:
+let heading = document.querySelector("h3");
+heading.innerHTML = "Updated heading";
+
+//Question 3:
+heading.style.fontSize = "2em";
+
+//Question 4:
+heading.classList.add("subheading");
+
+//Question 5:
+let paragraphs = document.querySelectorAll("p");
+paragraphs.forEach(paragraph => {
+    paragraph.style.color="red";
+});
+
+// You can also use a for loop:
+// for (let i = 0; i < paragraphs.length; i++) {
+//     paragraphs[i].style.color = "red";
+// };
+
+//Question 6:
+let resultsContainer = document.querySelector(".results");
+resultsContainer.innerHTML = "<p>New paragraph</p>";
+resultsContainer.style.cssText = "background-color:yellow;";
+
+//Question 7:
+function questionSeven(list) {
+    for (let i = 0; i < list.length; i++) {
+        console.log(list[i].name);
+    }
+    
+};
+questionSeven(cats);
+
+//Question 8:
+let catContainer = document.querySelector(".cat-container");
+let catsName = "";
+let catsAge = "";
+function createCats(cats) {
+    for (let i = 0; i < cats.length; i++) {
+        catsName = `<h5>${cats[i].name}</h5>`;
+        catsAge = `<p>${cats[i].age}</p>`;
+        if (!cats[i].age) {
+            catsAge = "<p>Age unknown</p>";
+        }
+        const finalHTML = `<div>${catsName}${catsAge}</div>`;
+        catContainer.innerHTML += finalHTML;
+    }
+};
+
+createCats(cats);
